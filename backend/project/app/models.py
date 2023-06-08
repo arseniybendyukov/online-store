39db90f85e05c432d959705c92ff5a5c89f7d3b5
PROFFClEANMARKET <proffcleanmarket@mail.ru>
2023-06-08 16:59:45 +0600

    Начало работы над моделями

--- backend/project/app/models.py
+++ backend/project/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+  name = models.CharField(max_length=100)
+  color = models.CharField(max_length=7)
+
+class Brand(models.Model):
+  name = models.CharField(max_length=100)
+  image = models.ImageField(upload_to='brands/')
+
+class Category(models.Model):
+  name = models.CharField(max_length=100)
+
+class Subcategory(models.Model):
+  name = models.CharField(max_length=100)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Product(models.Model):
+  name = models.CharField(max_length=100)
+  image = models.ImageField(upload_to='products/')
+  subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+  publications = models.ManyToManyField(Publication)
