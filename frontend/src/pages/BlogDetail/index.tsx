f9d7bd8d8f23b681a20d332d829d891e7a3087a1
PROFFClEANMARKET <proffcleanmarket@mail.ru>
2023-08-21 16:55:13 +0600

    /blog: детальная страница

--- frontend/src/pages/BlogDetail/index.tsx
+++ frontend/src/pages/BlogDetail/index.tsx
@@ -1,14 +1,32 @@
 import { useParams } from 'react-router-dom';
 import { useGetBlogDetailQuery } from '../../redux/apis/blogApi';
 import css from './index.module.css';
+import { Tag } from '../../components/Tag';
+import { monthAndDayFromDate } from '../../utils/data';
 
 export function BlogDetail() {
   const { id = '' } = useParams();
   const { data: blog, isLoading } = useGetBlogDetailQuery({ id });
 
-  console.log(blog);
+  return  isLoading ? <>Загрузка...</> : <>{blog && (
+    <div className={`container ${css.container}`}>
+      <div className={css.info}>
+        <div className={css.tags}>
+          {blog.tags.map((tag) => <Tag key={tag.id} name={tag.name} color={tag.color} />)}
+        </div>
 
-  return (
-    <div>Blog detail</div>
-  );
+        <span className={css.date}>{monthAndDayFromDate(blog.created_at)}</span>
+      </div>
+
+      <h1 className='h1'>{blog.heading}</h1>
+
+      <img src={blog.image} alt='blog' />
+
+      <p>{blog.description}</p>
+
+      <div className={css.separator}></div>
+
+      <p>{blog.text}</p>
+    </div>
+  )}</>;
 }
