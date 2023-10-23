7163e396d02920628a2923aae6ae56b69ddbfe6d
root <root@31-31-199-94.cloudvps.regruhosting.ru>
2023-10-23 19:28:10 +0300

    изменение адреса и почты

--- frontend/src/consts/data.ts
+++ frontend/src/consts/data.ts
@@ -1,6 +1,6 @@
 export const PHONE_NUMBER = '+7(913)-712-02-00';
-export const ADDRESS = 'г. Новосибирск, ул. Октябрьская, д. 42, офис 514';
+export const ADDRESS = 'г. Новосибирск, ул. Дуси Ковальчук, д. 28г';
 export const SCHEDULE = 'Пн-Пт с 11:00 до 18:00';
-export const EMAIL = 'proffcleanmarket@mail.ru';
-export const MAP_COORDS = [55.024394, 82.920016];
+export const EMAIL = 'cleanmarket54@gmail.com';
+export const MAP_COORDS = [55.050349, 82.897280];
 export const MAP_ZOOM = 16;
