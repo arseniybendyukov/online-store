1bbd5c36146b96bffbb38108086988ec38b39ab3
PROFFClEANMARKET <proffcleanmarket@mail.ru>
2024-01-24 22:16:49 +0600

    Добавил ссылки на соцсети

--- frontend/src/components/SocialMedias/index.tsx
+++ frontend/src/components/SocialMedias/index.tsx
@@ -1,29 +1,7 @@
-import { ReactNode } from 'react';
 import css from './index.module.css';
-import { ReactComponent as VK } from '../../images/social-medias/vk.svg';
-import { ReactComponent as Telegram } from '../../images/social-medias/telegram.svg';
-import { ReactComponent as Whatsapp } from '../../images/social-medias/whatsapp.svg';
 import { Link } from 'react-router-dom';
+import { socialMedias } from '../../consts/social-medias';
 
-interface SocialMedia {
-  path: string;
-  icon: ReactNode;
-}
-
-const socialMedias: SocialMedia[] = [
-  {
-    path: 'https://vk.com/proffcleanmarket',
-    icon: <VK width={40} height={40} />
-  },
-  {
-    path: '#',
-    icon: <Telegram width={30} height={30} style={{ transform: 'translate(-2px, 0)' }} />
-  },
-  {
-    path: '#',
-    icon: <Whatsapp width={35} height={35} />
-  },
-];
 
 interface Props {
   dark?: boolean;
