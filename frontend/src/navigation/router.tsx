8d62537210209045b990fabda851d1ea2e5485ed
PROFFCLEANMARKET <proffcleanmarket@mail.ru>
2024-05-24 23:05:39 +0500

    test

--- frontend/src/navigation/router.tsx
+++ frontend/src/navigation/router.tsx
@@ -32,6 +32,7 @@ import { BlogDetail } from '../pages/BlogDetail';
 import { EmailResend } from '../pages/Auth/EmailResend';
 import { About } from '../pages/About';
 import { HowToBuy } from '../pages/HowToBuy';
+import { Payment } from '../pages/Payment';
 
 export const router = createBrowserRouter([
   {
@@ -99,6 +100,10 @@ export const router = createBrowserRouter([
       {
         path: NavPaths.MAIN,
         element: <Main />
+      },
+      {
+        path: NavPaths.PAYMENT,
+        element: <Payment />
       },
       {
         path: NavPaths.ABOUT,
