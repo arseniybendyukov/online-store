93e86f59da109f76a8d7a2583cd8b305fd2425ae
PROFFCLEANMARKET <proffcleanmarket@mail.ru>
2024-05-27 16:31:24 +0500

    fix working hours

--- frontend/src/consts/data.ts
+++ frontend/src/consts/data.ts
@@ -1,6 +1,6 @@
 export const PHONE_NUMBER = '+7(913)-712-02-00';
 export const ADDRESS = 'г. Новосибирск, ул. Дуси Ковальчук, д. 28г';
-export const SCHEDULE = 'Пн-Пт с 11:00 до 18:00';
+export const SCHEDULE = 'Пн-Пт 10:00 — 18:00; Сб 10:00 — 14:00';
 export const EMAIL = 'cleanmarket54@gmail.com';
 export const MAP_COORDS = [55.050349, 82.897280];
 export const MAP_ZOOM = 16;
