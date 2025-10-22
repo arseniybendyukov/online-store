d883bf34d857d200dc265c4aff6164f366d18275
Arseniy Bendyukov <arseniy.bendyukov2006@mail.ru>
2025-10-22 22:30:28 +0200

    amount input max value

--- frontend/src/components/AmountInput/index.tsx
+++ frontend/src/components/AmountInput/index.tsx
@@ -27,11 +27,11 @@ export const AmountInput = ({ amount, setAmount, isBig = false }: Props) => (
       min='1'
       max='100'
       value={amount}
-      onChange={(e) => setAmount(Math.max(Number(e.target.value), 1))}
+      onChange={(e) => setAmount(Math.min(Math.max(Number(e.target.value), 1), 100))}
     />
     <button
       className={css.button}
-      onClick={() => setAmount((prevAmount) => prevAmount + 1)}
+      onClick={() => setAmount((prevAmount) => Math.min(prevAmount + 1, 100))}
     >
       <Increment />
     </button>
